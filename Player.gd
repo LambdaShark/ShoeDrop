56c0aa3e943a2d60e8888a03470e90decc25b439
Tomer Keren <tomer.keren.dev@gmail.com>
2020-10-08 22:50:39 +0300

    Camera look

--- Player.gd
+++ Player.gd
@@ -5,7 +5,13 @@ extends RigidBody
 # var a = 2
 # var b = "text"
 
+export onready var mouse_sensitivity : float = 0.2
+const TO_RAD = TAU / 90
 
-func _physics_process(delta:float):
-	pass
-	
+func _ready():
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+
+func _input(event: InputEvent):
+	if event is InputEventMouseMotion:
+		$Camera.rotation -= Vector3(event.relative.y, event.relative.x,0) * mouse_sensitivity * TO_RAD
+		$Camera.rotation.x = clamp($Camera.rotation.x, -TAU/8, TAU/8)
