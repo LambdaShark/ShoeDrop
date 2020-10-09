96f2f66c1ed101e536ce9b13fd395dedc04d0a30
Tomer Keren <tomer.keren.dev@gmail.com>
2020-10-09 10:24:06 +0300

    Move a ball

--- Player.gd
+++ Player.gd
@@ -1,17 +1,26 @@
-extends RigidBody
+extends KinematicBody
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
 
 export onready var mouse_sensitivity : float = 0.2
+export onready var throw_sensitivity : float = 0.01
 const TO_RAD = TAU / 90
 
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 
+enum State {
+	THROWING,
+	MOVING
+}
+
+var state = State.THROWING
+
 func _input(event: InputEvent):
 	if event is InputEventMouseMotion:
-		$Camera.rotation -= Vector3(event.relative.y, event.relative.x,0) * mouse_sensitivity * TO_RAD
-		$Camera.rotation.x = clamp($Camera.rotation.x, -TAU/8, TAU/8)
+		if state == State.MOVING:
+			$Camera.rotation_degrees -= Vector3(event.relative.y, event.relative.x,0) * mouse_sensitivity
+			$Camera.rotation.x = clamp($Camera.rotation.x, -TAU/8, TAU/8)
+		if state == State.THROWING:
+			$BallRotationPoint.rotate_object_local(Vector3(1,0,0),-event.relative.y * throw_sensitivity)
+			# $BallRotationPoint.rotate_object_local(Vector3(0,1,0),-event.relative.x * throw_sensitivity)
+
